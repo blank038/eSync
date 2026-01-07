d0e55260a05cc7f81d9ac719e166ba5f156e7b6b
Blank038 <admin@mc9y.com>
2026-01-07 12:01:23 +0800

    feat: 增加坐标模块的延迟传送

--- bukkit/src/main/kotlin/com/aiyostudio/esync/internal/module/entity/LocationEntity.kt
+++ bukkit/src/main/kotlin/com/aiyostudio/esync/internal/module/entity/LocationEntity.kt
@@ -7,6 +7,7 @@ import com.aiyostudio.esync.internal.plugin.EfficientSyncBukkit
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerTeleportEvent
 import java.util.logging.Level
 
 class LocationEntity : IEntity {
@@ -19,33 +20,36 @@ class LocationEntity : IEntity {
 
     override fun apply(player: Any): Boolean {
         try {
-            if (player is Player) {
-                val world = Bukkit.getWorld(worldName)
-                if (world != null) {
-                    val location = Location(world, x, y, z, yaw, pitch)
-                    player.teleport(location)
-                    
-                    // 输出传送日志
-                    EfficientSyncBukkit.instance.logger.info(
-                        "Player ${player.name} teleported to $worldName (${String.format("%.2f", x)}, ${String.format("%.2f", y)}, ${String.format("%.2f", z)})"
-                    )
-                    
-                    // 发送传送成功消息
-                    if (SyncConfig.isChatMessageEnabled()) {
-                        player.sendMessage(I18n.getStrAndHeader("sync.location.teleport-success"))
-                    }
-                } else {
-                    EfficientSyncBukkit.instance.logger.warning(
-                        "World '$worldName' not found for player ${player.name}'s saved location"
-                    )
-                    
-                    // 发送传送失败消息
-                    if (SyncConfig.isChatMessageEnabled()) {
-                        player.sendMessage(I18n.getStrAndHeader("sync.location.teleport-failed"))
-                    }
-                }
-                return true
+            if (player !is Player) {
+                return false
             }
+            val world = Bukkit.getWorld(worldName) ?: let {
+                EfficientSyncBukkit.instance.logger.warning(
+                    "World '$worldName' not found for player ${player.name}'s saved location"
+                )
+
+                // 发送传送失败消息
+                if (SyncConfig.isChatMessageEnabled()) {
+                    player.sendMessage(I18n.getStrAndHeader("sync.location.teleport-failed"))
+                }
+                return false
+            }
+            // 延迟传送玩家
+            Bukkit.getScheduler().runTaskLater(EfficientSyncBukkit.instance, {
+                val location = Location(world, x, y, z, yaw, pitch)
+                player.teleport(location, PlayerTeleportEvent.TeleportCause.PLUGIN)
+            }, 20L * EfficientSyncBukkit.instance.config.getInt("modules.location.delay"))
+
+            // 输出传送日志
+            EfficientSyncBukkit.instance.logger.info(
+                "Player ${player.name} teleported to $worldName (${String.format("%.2f", x)}, ${String.format("%.2f", y)}, ${String.format("%.2f", z)})"
+            )
+
+            // 发送传送成功消息
+            if (SyncConfig.isChatMessageEnabled()) {
+                player.sendMessage(I18n.getStrAndHeader("sync.location.teleport-success"))
+            }
+            return true
         } catch (e: Exception) {
             EfficientSyncBukkit.instance.logger.log(Level.WARNING, e) {
                 "Failed to apply 'location' data for ${(player as Player).uniqueId}"
